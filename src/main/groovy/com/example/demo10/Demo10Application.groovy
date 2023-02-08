eaa51d910cf9a4e13f47d7d3ffcb084e53ad39c7
mdrag <m.dragay@yandex.ru>
2023-02-08 15:16:56 +0300

    Initial commit

---
+++ src/main/groovy/com/example/demo10/Demo10Application.groovy
@@ -0,0 +1,13 @@
+package com.example.demo10
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Demo10Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(Demo10Application, args)
+    }
+
+}
