3dc529f4539878f34bc9e3c345802e0531b08b43
mdrag <m.dragay@yandex.ru>
2023-02-09 16:23:23 +0300

    task2.1

--- src/main/groovy/com/example/demo10/HW13/Book.java
+++ src/main/groovy/com/example/demo10/HW13/Book.java
@@ -6,38 +6,48 @@ public class Book {
     private final String bookName;
     private  int publishingYear;
     private final Author author;
-    public Book(String bookName, Author author, int publishingYear) {
-        this.bookName = bookName;
-        this.author = author;
-        this.publishingYear = publishingYear;
-    }
+
     public String getBookName() {
-        return this.bookName;
+        return bookName;
     }
+
     public int getPublishingYear() {
-        return this.publishingYear;
+        return publishingYear;
     }
-    public Author getAuthor() {
-        return author;
-    }
-    public void setPublishingYear(int publishingYear){
+
+    public void setPublishingYear(int publishingYear) {
         this.publishingYear = publishingYear;
     }
 
-    @Override
-    public String toString(){
-        return bookName + "; " + author + "; " + publishingYear;
+    public Author getAuthor() {
+        return author;
     }
-    @Override
-    public boolean equals(Object other) {
-        if (this.getClass() != other.getClass()) {
-            return false;
-        }
-        Book book = (Book) other;
-        return bookName.equals(book.getBookName()) && author.equals(book.getAuthor());
+
+    public Book(String bookName, int publishingYear, Author author) {
+        this.bookName = bookName;
+        this.publishingYear = publishingYear;
+        this.author = author;
     }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", publishingYear=" + publishingYear +
+                ", author=" + author +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publishingYear == book.publishingYear && Objects.equals(bookName, book.bookName) && Objects.equals(author, book.author);
+    }
+
     @Override
     public int hashCode() {
-        return Objects.hash(bookName, publishingYear);
+        return Objects.hash(bookName, publishingYear, author);
     }
 }
--- src/main/groovy/com/example/demo10/HW13/Main.java
+++ src/main/groovy/com/example/demo10/HW13/Main.java
@@ -3,24 +3,25 @@ package com.example.demo10.HW13;
 public class Main {
     public static void main(String[] args) {
         Author levTolstoy = new Author("Лев", "Толстой");
-        Book warAndPeace = new Book("Война и Мир", levTolstoy, 1867);
-        System.out.println("Автор - " + warAndPeace.getAuthor().getName() + " "
-                + warAndPeace.getAuthor().getSurname());
-        System.out.println("Книга - " + warAndPeace.getBookName());
-        System.out.println("Год выпуска - " + warAndPeace.getPublishingYear());
+        Book warAndPeace = new Book("Война и Мир", 1867, levTolstoy);
+        System.out.print("Автор - " + warAndPeace.getAuthor().getName() + " "
+                + warAndPeace.getAuthor().getSurname() +
+                "; Книга - " + warAndPeace.getBookName() +
+                "; Год выпуска - " + warAndPeace.getPublishingYear());
         System.out.println();
 
         Author aleksandrPushkin = new Author("Александр", "Пушкин");
-        Book prophet = new Book("Пророк", aleksandrPushkin, 1828);
-        Book prophet1 = new Book("Пророк", aleksandrPushkin, 1828);
+        Book prophet = new Book("Пророк", 1828, aleksandrPushkin);
+        Book prophet1 = new Book("Пророк", 1828, aleksandrPushkin);
+
 
         System.out.println("Автор - " + prophet.getAuthor().getName() + " "
                 + prophet.getAuthor().getSurname());
         System.out.println("Книга - " + prophet.getBookName());
         System.out.println("Год выпуска - " + prophet.getPublishingYear());
 
-        System.out.println(levTolstoy + " " + aleksandrPushkin);
-        System.out.println();
+        prophet.setPublishingYear(1829);
+        System.out.println("Год выпуска - " + prophet.getPublishingYear());
 
         System.out.println(warAndPeace);
         System.out.println();
@@ -29,5 +30,12 @@ public class Main {
         System.out.println(prophet.hashCode());
         System.out.println(prophet1.hashCode());
         System.out.println(warAndPeace.hashCode());
+        System.out.println();
+
+        System.out.println(levTolstoy + "; " + aleksandrPushkin);
+        System.out.println(levTolstoy.equals(aleksandrPushkin));
+        System.out.println(aleksandrPushkin.hashCode());
+        System.out.println(levTolstoy.hashCode());
+
     }
 }
